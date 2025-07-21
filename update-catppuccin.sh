8db96649b7d8b8126f1f8992242c23a0b73a7b7a
Tommy Duchesne <tommy.duchesne2@gmail.com>
2025-07-20 22:30:45 -0400

    add catppuccin themes and use the mocha flavor.
    Meow.

---
+++ update-catppuccin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-latte.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-frappe.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
